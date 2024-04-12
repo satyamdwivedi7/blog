785721e77650cbe76838c82ad2181292712c7cf0
Satyam Dwivedi <122256218+satyam7579@users.noreply.github.com>
2024-04-12 17:31:28 +0530

    app exported

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
+// const ejs = require("ejs");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -20,9 +20,7 @@ app.use(express.static("public"));
 const posts = [];
 
 app.get("/", function (req, res) {
-  res.render("home", { startingContent: homeStartingContent,
-    posts: posts
-   });
+  res.render("home", { startingContent: homeStartingContent, posts: posts });
 });
 
 app.get("/about", function (req, res) {
@@ -42,15 +40,17 @@ app.post("/compose", function (req, res) {
   posts.push(post);
   res.redirect("/");
 });
-app.get("/posts/:postName", function(req, res){
+app.get("/posts/:postName", function (req, res) {
   const requestedPost = req.params.postName;
-  posts.forEach(function(post){
-    if(requestedPost === post.Title){
-      res.render("post", {newTitle: post.Title, newPost: post.Body});
+  posts.forEach(function (post) {
+    if (requestedPost === post.Title) {
+      res.render("post", { newTitle: post.Title, newPost: post.Body });
     }
-  })
+  });
 });
 
 app.listen(3000 || process.env.PORT, function () {
   console.log("Server started on port 3000");
 });
+
+module.exports = app;
